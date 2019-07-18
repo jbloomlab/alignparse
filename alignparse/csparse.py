e5d58738f8d7833c0fd9979fa43f6e868c295dde
Kate Dusenbury <khdusenbury@gmail.com>
2019-07-18 15:01:25 -0700

    Can parse features at beginning of alignment
    Many errors and edge cases to fix

--- alignparse/csparse.py
+++ alignparse/csparse.py
@@ -10,7 +10,7 @@ import re
 
 CS_OPERATORS = (':', '*', '+', '-', '~', '=')
 
-"""list: characters used as operators in the ``cs`` tag.
+"""tuple: characters used as operators in the ``cs`` tag.
 
 From https://lh3.github.io/minimap2/minimap2.html#10
 
@@ -47,7 +47,7 @@ def get_ambiguous(ambiguousDNA):
         raise ValueError('No ambiguous nucleotide.')
 
 
-def cs_list(cs, cs_splits):
+def cs_list(cs, *, cs_splits=CS_OPERATORS):
     """
     Turn a ``cs`` tag string into a list of component tags.
 
@@ -123,7 +123,10 @@ def convert_len(tag):
     elif re.match(r'^\+', tag):
         return 0
     elif re.match(r'^\-', tag):
-        return (len(tag)-1)
+        if re.match(r'^\-\d+', tag):
+            return int(re.findall(r'\d+', tag)[0])
+        else:
+            return (len(tag)-1)
     elif re.match(r'^\=', tag):
         return(len(tag)-1)
     elif re.match(r'^\~', tag):
@@ -147,16 +150,95 @@ def split(cs, alignstart, alignend, featstart, featend):
 
     Returns
     -------
-    split_tag : str
-        ``cs`` tag for region specified by ref_indices
+    feat_cs : str
+        ``cs`` tag for region specified by featstart and featend
 
     """
     # return None if feature coordinates not in alignment
-    if featstart <= alignstart and featend <= alignstart:
+    if featend <= alignstart:
         return None
-    elif featstart >= alignend and featend >= alignend:
+    elif featstart >= alignend:
         return None
-    # else:
+
+    else:
+        feat_cs = ''
+        ref_loc = alignstart
+        cslist = cs_list(cs)
+        csindex = 0
+
+        # if feature starts at beginning of alignment
+        if featstart == ref_loc:
+            ref_loc += convert_len(cslist[csindex])
+            while ref_loc < featend:
+                feat_cs += cslist[csindex]
+                csindex += 1
+                ref_loc += convert_len(cslist[csindex])
+            if ref_loc == featend:
+                feat_cs += cslist[csindex]
+            elif cslist[csindex][0] == ':':
+                feat_cs = feat_cs + ':' + str(featend - (ref_loc -
+                                                convert_len(cslist[csindex])))
+            elif cslist[csindex][0] == '+' or cslist[csindex][0] == '-':
+                raise RuntimeError('Breaks at indels not yet implemented.')
+
+        # if feature starts before alignment, but overlaps with alignment
+        elif featstart < ref_loc and featend >= ref_loc:
+            feat_cs = '-{0}'.format(ref_loc - featstart)
+            ref_loc += convert_len(cslist[csindex])
+            while ref_loc < featend:
+                feat_cs += cslist[csindex]
+                csindex += 1
+                ref_loc += convert_len(cslist[csindex])
+            if ref_loc == featend:
+                feat_cs += cslist[csindex]
+            elif cslist[csindex][0] == ':':
+                feat_cs = feat_cs + ':' + str(featend - (ref_loc -
+                                                convert_len(cslist[csindex])))
+
+        # if feature starts after alignment starts
+        elif featstart > ref_loc:
+            ref_loc += convert_len(cslist[csindex])
+            while ref_loc < featstart:
+                csindex += 1
+                ref_loc += convert_len(cslist[csindex])
+            # if feature starts between cs tags
+            if featstart == ref_loc:
+                csindex += 1
+                ref_loc += convert_len(cslist[csindex])
+                while ref_loc < featend:
+                    feat_cs += cslist[csindex]
+                    csindex += 1
+                    ref_loc += convert_len(cslist[csindex])
+                if ref_loc == featend:
+                    feat_cs += cslist[csindex]
+                if cslist[csindex][0] == ':':
+                    feat_cs = feat_cs + ':' + str(featend - (ref_loc -
+                                                convert_len(cslist[csindex])))
+            # if feature starts in middle of match, get length of match first
+            # THIS IS WRONG
+            elif ref_loc > featstart:
+                csindex += 1
+                if cslist[csindex-1][0] == ':':
+                    feat_cs += feat_cs + ':' + str(ref_loc - featstart)
+                while ref_loc <= featend:
+                    feat_cs += cslist[csindex]
+                    csindex += 1
+                    ref_loc += convert_len(cslist[csindex])
+                if cslist[csindex][0] == ':':
+                    feat_cs = feat_cs + ':' + str(featend - (ref_loc -
+                                                convert_len(cslist[csindex])))
+
+            # Need to implement if feat start and end are in last tag.
+
+    feat_list = cs_list(feat_cs)
+    feat_len = 0
+    for feat in feat_list:
+        feat_len += convert_len(feat)
+
+    if feat_len != (featend - featstart):
+        raise ValueError('`feat_cs` incorrect length.')
+
+    return feat_cs
 
 
 if __name__ == '__main__':
