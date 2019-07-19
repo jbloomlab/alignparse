5a3b766a14bd65c889ad633009f78723363c2db9
jbloom <jbloom@fredhutch.org>
2019-07-19 16:20:33 -0700

    regex parsing of `cs` strings

--- alignparse/cs_tag.py
+++ alignparse/cs_tag.py
@@ -3,12 +3,117 @@
 cs_tag
 =======
 
-Parse SAM entries with ``cs`` tag from ``minimap2`` output.
+Parse SAM entries with ``cs`` short tag from ``minimap2`` output.
+
+See here for details on short ``cs`` tag format:
+https://lh3.github.io/minimap2/minimap2.html
 
 """
 
 
-import re
+import regex
+
+
+_CS_OPS = {
+    'identity': ':[0-9]+',
+    'substitution': r'\*[acgtn][acgtn]',
+    'insertion': r'\+[acgtn]+',
+    'deletion': r'\-[acgtn]+',
+    }
+"""dict: Short ``cs`` tag operation regular expression matches."""
+
+_CS_STR_REGEX = regex.compile('(' + '|'.join(list(_CS_OPS.values())) + ')+')
+"""regex.Regex: matches full-length short ``cs`` tags."""
+
+_CS_OP_REGEX = regex.compile('|'.join(f"(?P<{op_name}>{op_str})" for
+                                      op_name, op_str in _CS_OPS.items()))
+"""regex.Regex: matches single ``cs`` operation, group name is operation."""
+
+
+def split_cs(cs_string, *, invalid='raise'):
+    """Split a short ``cs`` tag into its constituent operations.
+
+    Parameters
+    ----------
+    cs_string : str
+        The short ``cs`` tag.
+    invalid : {'raise', 'ignore'}
+        If `cs_string` is not a valid string, raise an error or ignore it
+        and return `None`.
+
+    Return
+    ------
+    list or None
+        List of the individual ``cs`` operations, or `None` if invalid
+        `cs_string` and `invalid` is 'ignore'.
+
+    Example
+    -------
+    >>> split_cs(':32*nt*na:10-gga:5+aaa:10')
+    [':32', '*nt', '*na', ':10', '-gga', ':5', '+aaa', ':10']
+
+    >>> split_cs('bad:32*nt*na:10-gga:5', invalid='ignore') is None
+    True
+
+    >>> split_cs('bad:32*nt*na:10-gga:5')
+    Traceback (most recent call last):
+    ...
+    ValueError: invalid `cs_string` of bad:32*nt*na:10-gga:5
+
+    """
+    m = _CS_STR_REGEX.fullmatch(cs_string)
+    if m is None:
+        if invalid == 'ignore':
+            return None
+        elif invalid == 'raise':
+            raise ValueError(f"invalid `cs_string` of {cs_string}")
+        else:
+            raise ValueError(f"invalid `invalid` of {invalid}")
+    else:
+        return m.captures(1)
+
+
+def cs_op_type(cs_op, *, invalid='raise'):
+    """Get type of ``cs`` operation.
+
+    Parameters
+    ----------
+    cs_op : str
+        A **single** operation in a short ``cs`` tag.
+    invalid : {'raise', 'ignore'}
+        If `cs_string` is not a valid string, raise an error or ignore it
+        and return `None`.
+
+    Returns
+    -------
+    {'substitution', 'insertion', 'deletion', 'identity', None}
+        Type of ``cs`` operation, or `None` if `cs_string` invalid and
+        `invalid` is 'ignore'.
+
+    Example
+    -------
+    >>> cs_op_type('*nt')
+    'substitution'
+    >>> cs_op_type(':45')
+    'identity'
+    >>> cs_op_type('*nt:45')
+    Traceback (most recent call last):
+    ...
+    ValueError: invalid `cs_op` of *nt:45
+    >>> cs_op_type('*nt:45', invalid='ignore') is None
+    True
+
+    """
+    m = _CS_OP_REGEX.fullmatch(cs_op)
+    if m is None:
+        if invalid == 'ignore':
+            return None
+        elif invalid == 'raise':
+            raise ValueError(f"invalid `cs_op` of {cs_op}")
+        else:
+            raise ValueError(f"invalid `invalid` of {invalid}")
+    else:
+        return m.lastgroup
 
 
 class Alignment:
@@ -17,7 +122,9 @@ class Alignment:
     Parameters
     ----------
     sam_alignment : pysam.AlignedSegment
-        Aligned segment from `pysam <https://pysam.readthedocs.io>`_.
+        Aligned segment from `pysam <https://pysam.readthedocs.io>`_,
+        must have a short format ``cs`` tag (see
+        https://lh3.github.io/minimap2/minimap2.html).
 
     Attributes
     ----------
@@ -37,10 +144,11 @@ class Alignment:
         Last position of alignment in target (exclusive).
 
     """
+
     def __init__(self, sam_alignment):
         """See main class docstring."""
         self.query_name = sam_alignment.query_name
-        self.target_name = same_alignment.reference_name
+        self.target_name = sam_alignment.reference_name
         self.query_clip5 = sam_alignment.query_alignment_start
         self.query_clip3 = (sam_alignment.query_length -
                             sam_alignment.query_alignment_end)
@@ -54,7 +162,6 @@ class Alignment:
             self.cs = sam_alignment.get_tag('cs')
         else:
             raise ValueError(f"alignment {self.name} has no `cs` tag")
-
 
     def extract_cs(self, start, end, *,
                    max_clip5=0, max_clip3=0):
@@ -79,7 +186,7 @@ class Alignment:
             If region of target covered by feature is aligned, return
             str with ``cs`` tag for this portion of alignment. If that region
             is not covered (even adding any padding from `max_clip5` /
-            `max_clip3`), return `None.
+            `max_clip3`), return `None`.
 
         """
         # make sure we have indexing correct (0- or 1-based)
